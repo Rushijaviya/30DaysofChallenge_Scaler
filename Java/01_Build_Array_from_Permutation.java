9ef0cb38924174dcd051ea74f4054f877e291584
Deepanshu Manocha <dmanocha464@gmail.com>
2022-09-26 20:44:23 +0530

    01 Build Array from Permutation
    Signed-off-by: Deepanshu Manocha <dmanocha464@gmail.com>

---
+++ Java/01_Build_Array_from_Permutation.java
@@ -0,0 +1,31 @@
+/*
+ * 1920. Build Array from Permutation
+ * link: https://leetcode.com/problems/build-array-from-permutation/
+ */
+
+//approach 1 TC: O(n) SC: O(n)
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
+
+//approach 2 TC: O(n) SC: O(1)
+
+class Solution2 {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            nums[i] = (nums[nums[i]] % n) * n + nums[i]; // (new value)*n + (old value)
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] /= n;
+        }
+        return nums;
+    }
+}
